e86465581bbbfa83bfe21bf723419eb33dfba4b3
U-BSD\517459 <517459@SHS-JYBF6X3.beaverton.k12.or.us>
2025-10-16 09:20:09 -0700

    Made you be able to use it

--- studentList.cpp
+++ studentList.cpp
@@ -27,26 +27,31 @@ int main()
   bool quit = false; //Flip to quit
   char actions[4][7] = {"ADD", "DELETE", "PRINT", "QUIT"}; //Allowed actions to do
 
+  //DEBUG
+  //int iterations = 0;
+  
   //Loops and asks for actions each time
   while(!quit)
     {
       //Quit
       if(quit) {return 0;}
-      
+  
       //Get action
       cout << "What is your action? (ADD, DELETE, PRINT, or QUIT)" << &endl;
       char action[6];
       cin.get(action, 7);
-      cin.clear();
 
-      //Check which action
-      cout << "Debug. Action: " << action << &endl;
+      //Print which action
+      //cout << "Debug; action: " << action << &endl;
+
       if(strcmp(action, actions[0]) == 0) {add(studentList);} //Add  
       else if(strcmp(action, actions[1]) == 0) {deleteStudent(studentList);} //Delete
       else if(strcmp(action, actions[2]) == 0) {printStudents(studentList);} //Print
       else if(strcmp(action, actions[3]) == 0) {quit = !quit;} //Quit
-      else {action[0] = '\0';} //Reset action
-      return 0;
+
+      action[0] = '\0'; //Reset action
+
+      //iterations++;
     }
 }
 
@@ -55,19 +60,23 @@ void add(vector<student*> &vect)
   //Get input
   char firstName[30];
   cout << "What is the first name of the student to add?" << &endl;
+  cin.ignore();
   cin.get(firstName, 31);
 
   char lastName[30];
   cout << "What is the last name of the student to add?" << &endl;
+  cin.ignore();
   cin.get(lastName, 31);
-
+  
   int studentID = 0;
   cout << "What is the ID of the student to add?" << &endl;
   cin >> studentID;
-
+  cin.ignore();
+  
   float GPA = 0.0;
   cout << "What is the GPA of the student to add?" << &endl;
   cin >> GPA;
+  cin.ignore();
 
   //Make struct
   student* newStudent = new student;
@@ -88,6 +97,7 @@ void printStudents(vector<student*> vect)
       cout << fixed << setprecision(2);
       cout << (*vect[i]).firstName << " " << (*vect[i]).lastName << ", " << (*vect[i]).studentID << ", " << (*vect[i]).GPA << &endl;
     }
+  cin.ignore();
 }
   
 void deleteStudent(vector<student*> &vect)
@@ -95,6 +105,7 @@ void deleteStudent(vector<student*> &vect)
   int ID = 0;
   cout << "What is the ID of the student to delete?" << &endl;
   cin >> ID;
+  cin.ignore();
   
   for(int i = 0; i < vect.size(); i++)
     {
